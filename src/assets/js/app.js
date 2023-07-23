2e6c206e4a145a31753545e5e7376255bfe9f1d6
Bryce Fisher <bryce.e.fisher@gmail.com>
2023-07-22 21:19:39 -0700

    Added responsivness to home page

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,32 +1,41 @@
 import "./general";
 
-function togglePhotos() {
-  const img2 = document.querySelector("#img2");
-  const img4 = document.querySelector("#img4");
-  const img6 = document.querySelector("#img6");
-  const img8 = document.querySelector("#img8");
+class NAACM {
+  constructor() {
+    this.setCurrentYear();
+    this.addEventListeners();
+  }
 
-  if (window.innerWidth < 768) {
-    img2.classList.add("visually-hidden");
-    img4.classList.add("visually-hidden");
-    img6.classList.add("visually-hidden");
-    img8.classList.add("visually-hidden");
-  } else {
-    img2.classList.remove("visually-hidden");
-    img4.classList.remove("visually-hidden");
-    img6.classList.remove("visually-hidden");
-    img8.classList.remove("visually-hidden");
+  // Removes 4 photos from the gallery on mobile
+  togglePhotos() {
+    const img2 = document.querySelector("#img2");
+    const img4 = document.querySelector("#img4");
+    const img6 = document.querySelector("#img6");
+    const img8 = document.querySelector("#img8");
+
+    if (window.innerWidth < 768) {
+      img2.classList.add("visually-hidden");
+      img4.classList.add("visually-hidden");
+      img6.classList.add("visually-hidden");
+      img8.classList.add("visually-hidden");
+    } else {
+      img2.classList.remove("visually-hidden");
+      img4.classList.remove("visually-hidden");
+      img6.classList.remove("visually-hidden");
+      img8.classList.remove("visually-hidden");
+    }
+  }
+
+  addEventListeners() {
+    window.addEventListener("resize", this.togglePhotos);
+  }
+
+  setCurrentYear() {
+    const date = document.querySelector("#date");
+    date.innerHTML = new Date().getFullYear();
   }
 }
 
-function addEventListeners() {
-  window.addEventListener("resize", togglePhotos);
-}
-
-function setCurrentYear() {
-  const date = document.querySelector("#date");
-  date.innerHTML = new Date().getFullYear();
-}
-
-setCurrentYear();
-addEventListeners();
+window.addEventListener("DOMContentLoaded", () => {
+  new NAACM();
+});
